1fdc58fb1450125c7328a24aca7d6ff581043cc2
cvargasr31 <cvargasr.31@gmail.com>
2021-06-24 17:59:14 -0500

    updated things

--- src/test/java/stepdefinitions/Hooks.java
+++ src/test/java/stepdefinitions/Hooks.java
@@ -3,9 +3,7 @@ package stepdefinitions;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
-import io.qameta.allure.Description;
 import opencart.Base;
-
 import org.openqa.selenium.WebDriver;
 import pageobjects.MainPage;
 
@@ -15,14 +13,12 @@ public class Hooks extends Base {
     public static WebDriver driver;
 
     @Before
-    @Description("before hook")
     public void before(){
         driver = initializeDriver();
         driver.get(MainPage.getUrl());
     }
 
     @After
-    @Description("after hook")
     public void after(Scenario scenario){
         if(scenario.isFailed()){
             try {
